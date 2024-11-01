f72dbf0c757b851a3573e232211bad8b03062ab8
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-11-01 00:14:47 -0300

    primeiro commit

---
+++ testegateway/src/main/java/gustavo/teste/testegateway/TestegatewayApplication.java
@@ -0,0 +1,13 @@
+package gustavo.teste.testegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestegatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestegatewayApplication.class, args);
+	}
+
+}
---
+++ testegateway/src/test/java/gustavo/teste/testegateway/TestegatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package gustavo.teste.testegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestegatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
